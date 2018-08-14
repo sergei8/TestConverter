a41bbca3977506ebee19c0f20012e75efb36c36e
Serhii Shkliarskiy <shkliarskiy@gmail.com>
2018-08-14 22:03:06 +0300

    fix config

--- src/app/shared/config.service.ts
+++ src/app/shared/config.service.ts
@@ -1,15 +1,13 @@
 import {Injectable} from '@angular/core';
 
-/**
- * Настройка параметров приложения
- */
 @Injectable()
 export class ConfigService {
-  public answersNumber: number;   // количестов ответов в тесте
-  public semanticCheck: boolean;  // делать ли семантический анализ
+  public answersNumber: number;  // количество ответов
+  public semanticCheck: boolean; // семантическая проверка
 
   constructor() {
     this.answersNumber = 4;
+    this.semanticCheck = false;
   }
 }
 
