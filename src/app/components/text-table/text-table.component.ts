7272ffc14ac289ec88fb7d930b2cacd7c17ceef8
Serhii Shkliarskiy <shkliarskiy@gmail.com>
2018-08-14 22:09:31 +0300

    fix text-table component

--- src/app/components/text-table/text-table.component.ts
+++ src/app/components/text-table/text-table.component.ts
@@ -1,4 +1,4 @@
-import {Component, DoCheck} from '@angular/core';
+import {Component} from '@angular/core';
 import {DataService} from '../../shared/data-service';
 
 @Component({
@@ -6,13 +6,13 @@ import {DataService} from '../../shared/data-service';
   templateUrl: './text-table.component.html',
   styleUrls: ['./text-table.component.css']
 })
-export class TextTableComponent implements  DoCheck {
+export class TextTableComponent {
   public plainTestsList;
 
   constructor(private dataservice: DataService) {
   }
 
   ngDoCheck() {
-    this.plainTestsList = this.dataservice.testsList;
+    // this.plainTestsList = this.dataservice.testsList;
   }
 }
